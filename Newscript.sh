ad00f8a066a6707ec0bd1bb4d9a6be42b434bf5e
GomathiBose <90387781+GomathiBose@users.noreply.github.com>
2022-12-05 13:23:32 +0530

    Update Newscript.sh

--- Newscript.sh
+++ Newscript.sh
@@ -3,36 +3,20 @@
 
 set -x
 git status --porcelain
-git fetch 
-declare -a LOCAL_BRANCH=$(git branch -l | cut -c3- | tr "\n" " ")
+git fetch
+declare -a LOCAL_BRANCH=$(git branch -l | cut  -d " " -f 2)
+
 REMOTE_BRANCH=$(git rev-parse --abbrev-ref --symbolic-full-name $LOCAL_BRANCH@{u})
 
 REMOTE_COMMITS=$(git log --oneline $LOCAL_BRANCH..$REMOTE_BRANCH | wc -l)
 
 LOCAL_COMMITS=$(git log --oneline $REMOTE_BRANCH..$LOCAL_BRANCH | wc -l)
-CURR_BRANCH=$(git branch | grep '*' | cut -c3-)
+CURR_BRANCH=$(git branch | cut -d " " -f 2)
 if [ $REMOTE_COMMITS != "0" ]; then
-	if [ "$CURR_BRANCH" = "$LOCAL_BRANCH" ]; then
-		if [ "$(git status --porcelain)" = "" ]; then
-			git pull
-			echo 'git pull done'
-		fi
-	else
-		git fetch origin $LOCAL_BRANCH:$LOCAL_BRANCH
-	fi
+        echo 'inside git pull'
+        git status --porcelain
+        git pull
+        echo 'git pull done'
+else
+        echo 'no change required'
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
