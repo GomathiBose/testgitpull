cc53f2ae3d9f2b53bd45c308a42b99ad86eea436
gomathibose <gomathi@popup-mainframe.com>
2022-12-07 16:25:09 +0530

    tets commit

--- gitpullscript.sh
+++ gitpullscript.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-set -x
-git status --porcelain
+# set -x
+git status -b
 git fetch
-LOCAL_BRANCH=$(git branch -l | cut  -d " " -f 2)
-
+LOCAL_BRANCH=$(git branch | cut  -d " " -f 2)
 REMOTE_BRANCH=$(git rev-parse --abbrev-ref --symbolic-full-name $LOCAL_BRANCH@{u})
-
 REMOTE_COMMITS=$(git log --oneline $LOCAL_BRANCH..$REMOTE_BRANCH | wc -l)
 
 if [ $REMOTE_COMMITS != "0" ]; then
-        echo 'inside git pull'
-        git status --porcelain
-        git pull
-        echo 'git pull done'
+        echo 'Action Required! Perform Git pull to make remote changes available to your local'
 else
-        echo 'no change required'
+	echo 'no action required'
 fi
-
-
 
